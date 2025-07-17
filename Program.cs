d7c97750fbdf4b86709d881fa89a0b4cb252153d
minmax7 <minhmoc2509@gmail.com>
2025-07-18 00:08:38 +0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using QLKS_WebMVC.Data;
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddDbContext<QLKS_WebMVCContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("QLKS_WebMVCContext") ?? throw new InvalidOperationException("Connection string 'QLKS_WebMVCContext' not found.")));
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+}
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapStaticAssets();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}")
+    .WithStaticAssets();
+
+
+app.Run();
