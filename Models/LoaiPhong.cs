cf699d5d5979564c905385b67c10babe6f66fa7e
Rapnee <caobagiap2004@gmail.com>
2025-07-20 00:28:38 +0700

    Add files via upload

---
+++ Models/LoaiPhong.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.EntityFrameworkCore;
+
+namespace QLKS_WebMVC.Models
+{
+    public class LoaiPhong
+    {
+        [Key]
+
+        [Display(Name = "Mã loại phòng")]
+        public int Id { get; set; }
+
+        [Required]
+        [Display(Name = "Tên loại phòng")]
+        public string? TenLoai {  get; set; }
+
+        [Required]
+        [Display(Name = "Gía phòng")]
+        [Precision(18, 2)]
+        public decimal GiaPhong { get;set; }
+
+        [Display(Name ="Mô tả")]
+        public string? MoTa {  get; set; }
+
+        [Display(Name = "Hình ảnh")]
+        public string? HinhAnh { get; set; }
+        public ICollection<Phong>? Phongs { get; set; }
+
+    }
+}
---
+++ Models/Phong.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace QLKS_WebMVC.Models
+{
+    public class Phong
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [Display(Name = "Tên phòng")]
+        public string? TenPhong { get; set; }
+        [Required]
+        [Display(Name = "Trạng thái")]
+        public string? TrangThai { get;set; } //tr
+
+        [Display(Name = "Loại phòng")]
+        public int LoaiPhongId {  get; set; }
+
+        [ForeignKey("LoaiPhongId")]
+        public LoaiPhong? LoaiPhong { get; set; }    
+    }
+}
